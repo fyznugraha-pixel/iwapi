2bf92501cc41d1f483f7207d7fcd1880e34ee9f1
kelompok 1 <fyznugraha@gmail.com>
2026-07-14 13:08:41 +0700

    Config: Add Multi-Zones rewrites for portfolio

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/portofolio',
+        destination: 'https://portfolio-fayiz-pied.vercel.app',
+      },
+      {
+        source: '/portofolio/:path*',
+        destination: 'https://portfolio-fayiz-pied.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
